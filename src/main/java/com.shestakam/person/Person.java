644f59efc481b34b1b0c7bd04fc1fcabc0ff57a6
AlexandrShestak <shestakalexandr@mail.ru>
2016-05-30 21:52:38 +0300

    add spring security, delete lombok (it is strange library)

--- src/main/java/com.shestakam/person/Person.java
+++ src/main/java/com.shestakam/person/Person.java
@@ -1,16 +1,29 @@
 package com.shestakam.person;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Data
 public class Person {
     @Id
     @GeneratedValue
     private Long id;
     private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
