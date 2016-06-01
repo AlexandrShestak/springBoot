d66f888630fed449b05001d8edf84baaea39ffdd
AlexandrShestak <shestakalexandr@mail.ru>
2016-06-01 21:30:11 +0300

    add basePackageClasses for PersistenceConfiguration

--- src/main/java/com.shestakam/configuration/PersistenceConfiguration.java
+++ src/main/java/com.shestakam/configuration/PersistenceConfiguration.java
@@ -1,11 +1,12 @@
 package com.shestakam.configuration;
 
+import com.shestakam.person.PersonRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension;
 
 @Configuration
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackageClasses = {PersonRepository.class})
 public class PersistenceConfiguration extends JpaRepositoryConfigExtension {
 // I added some code to put two persons into the database here.
 }
